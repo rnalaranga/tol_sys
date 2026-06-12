52593e43f5959d0dd8d0bb3b4c97b2f6924b743b
rnalaranga <nalaranga@gmail.com>
2026-06-12 21:47:15 +0530

    Fix API base URL and local updates

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
